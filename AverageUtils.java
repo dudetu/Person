176f5aa382b4e545c7945771b338e6fea5b64699
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-01-11 18:54:14 +0100

    Add files via upload

---
+++ AverageUtils.java
@@ -0,0 +1,11 @@
+public class AverageUtils {
+
+    public static  double averageOfThreeNumbers( double first,double second,double third){
+       return (first + second + third) / 3 ;
+    }
+
+    public static  double averageOfTwooNumbers( double first,double second){
+        return (first + second ) / 2 ;
+    }
+
+}
