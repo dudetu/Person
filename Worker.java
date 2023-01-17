c13439537d9f07248867d4e3c4d9f2f4f900b1e5
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-01-18 00:54:03 +0100

    Add files via upload

--- Worker.java
+++ Worker.java
@@ -1,27 +1,49 @@
-public class Worker extends Person{
+import static jdk.internal.icu.lang.UCharacter.getAge;
 
-  private   double minSalaary;
-   private double maxSalary;
+public class Worker extends Person implements AbleToCalculatePension {
 
-@Override
-   public void die(){
-       System.out.println("Этот человек не дожил до пенсии");
+    private double minSalaary;
+    private double maxSalary;
 
-   }
+    @Override
+    public void die() {
+        System.out.println("Этот человек не дожил до пенсии");
+
+    }
 
     public double getMinSalaary() {
+
         return minSalaary;
     }
 
     public void setMinSalaary(double minSalaary) {
+
         this.minSalaary = minSalaary;
     }
 
     public double getMaxSalary() {
+
         return maxSalary;
     }
 
     public void setMaxSalary(double maxSalary) {
+
         this.maxSalary = maxSalary;
     }
+
+
+    @Override
+    public double calculatePension() {
+        PensionFund pensionFund = new PensionFund(" Пенсионный фонд Берлин", true, "28-05-1977");
+        int age = getAge();
+      double result = pensionFund.calculatePension(age, maxSalary, minSalaary);
+        return result;
+
+
+    }
+
+
+
+
+
 }
