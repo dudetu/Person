7155ba465dadad1518ce219235a7e17883e7e959
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-02-05 23:42:39 +0100

    Add files via upload

--- Worker.java
+++ Worker.java
@@ -1,6 +1,11 @@
-import static jdk.internal.icu.lang.UCharacter.getAge;
+import java.util.List;
 
-public class Worker extends Person implements AbleToCalculatePension {
+public class Worker  extends Person implements AbleToCalculatePension {
+private static final int MONEY_PER_CHILD = 200;
+
+    public Worker(List<Person> children) {
+        super(children);
+    }
 
     private double minSalaary;
     private double maxSalary;
@@ -36,14 +41,25 @@ public class Worker extends Person implements AbleToCalculatePension {
     public double calculatePension() {
         PensionFund pensionFund = new PensionFund(" Пенсионный фонд Берлин", true, "28-05-1977");
         int age = getAge();
-      double result = pensionFund.calculatePension(age, maxSalary, minSalaary);
+        int additionalSalary = 0;
+         if ( getChildren() != null){
+             additionalSalary = getChildren().size() * MONEY_PER_CHILD;
+         }
+        additionalSalary += minSalaary;
+        
+        double result = pensionFund.calculatePension(age, maxSalary, minSalaary);
         return result;
-
-
+        
     }
-
-
-
+  public String toString (){
+      String month = null;
+      return  "classes.Worker{"+
+              "minSalary="+ minSalaary + 
+                " ,maxSalary=" + maxSalary +
+                ", month=" + month +
+                '}';
+                
+  }
 
 
 }
