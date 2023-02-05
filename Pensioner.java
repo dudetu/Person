14a317307758b3eff610881d11ccbcf558b43932
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-02-05 23:44:45 +0100

    Add files via upload

--- Pensioner.java
+++ Pensioner.java
@@ -1,17 +1,51 @@
+
+
 public class Pensioner extends Person{
 
-      private double pension;
 
-@Override
-       public void die(){double x = (getAge() -50)*pension;
-        System.out.println("Этот пенсионер умер, он заработал: ");
+    private double pension;
+
+    public Pensioner(String name, int age, int height, double weight , double pension){
+        super(name,age,height, (int) weight);
+        this.pension = pension;
+    }
+
+    @Override
+    public  void  die (){
+        System.out.println( "и , что-то произошло ...");
+        super.die();
+        System.out.println( "Ага, вот так и случилось!");
+
+
+        int countOfChildren = 0;
+        if ( getChildren() != null){
+            countOfChildren = getChildren().size();
+        }
+
+        System.out.println( "Зато у меня "+ countOfChildren + "Детей");
+    }
+     //  public void die(){double x = (getAge() -50)*pension;
+     //  System.out.println("Этот пенсионер умер, он заработал: ");
+      // }
+
+
+       public void abc ( int a ){
+           System.out.println( a);
        }
 
+    public int getAge() {
+
+        return 0;
+    }
+
     public double getPension() {
-        return pension;
+
+    return pension;
     }
 
     public void setPension(double pension) {
-        this.pension = pension;
+
+    this.pension = pension;
     }
 }
+
