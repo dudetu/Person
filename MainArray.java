0717d02bfdf0bc3c20fe5616dce2f57654998992
Larysa Bilan <114293094+dudetu@users.noreply.github.com>
2023-02-11 14:06:21 +0100

    Add files via upload

---
+++ MainArray.java
@@ -0,0 +1,29 @@
+package Human;
+
+import java.util.ArrayList;
+
+public class MainArray {
+
+    public static  void main(String[]args) {
+
+       ArrayListImplementation list = new ArrayListImplementation(10);
+        ArrayList<Integer> arrayList = new ArrayList<>(10);
+
+        list.add(1);
+        list.add(12);
+        list.add(45);
+        list.add(67);
+
+        list.remove(2);
+        list.print();
+    }
+
+
+
+
+}
+
+
+
+
+
